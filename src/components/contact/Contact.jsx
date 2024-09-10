c41c19d88afdb4d1ab7c8c148225660afb917997
Bijay Baniya <bijaybaniya1088@gmail.com>
2024-09-10 19:20:44 +0100

    Added SEO

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -15,7 +15,7 @@ const Contact = () => {
         <ul>
           <li><FontAwesomeIcon icon={faPhone} className='me-2'/><a href="tel:+44 07778794590">+44 07778794590</a></li>
           <li><FontAwesomeIcon icon={faEnvelope} className='me-2'/><a href="mailto:bijaybaniya1088@gmail.com">bijaybaniya1088@gmail.com</a></li>
-          <li><FontAwesomeIcon icon={faMapMarkerAlt} className='me-2'/>Feltham, London<br/>UK</li>
+          <li><FontAwesomeIcon icon={faMapMarkerAlt} className='me-2'/>London, UK<br/>UK</li>
         </ul>
       </div>
     </div>
